dd19543bf5251571207cd2695e0456252271a951
Kelson Palharini <kelsonpalharini@bsd.com.br>
2020-10-18 17:29:40 -0300

    Adjustments on AuthContext file

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 
-import SignIn from './pages/SignIn';
-// import SignUp from './pages/SignUp';
+import { AuthProvider } from './hooks/AuthContext';
 
-const App: React.FC = () => (
-  <>
-    <SignIn />
-    <GlobalStyle />
-  </>
-);
+import SignIn from './pages/SignIn';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
+
+      <GlobalStyle />
+    </>
+  );
+};
 
 export default App;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -12,30 +12,46 @@ import Input from '../../components/Input';
 import { Container, Content, Background } from './styles';
 
 import getValidationErrors from '../../utils/getValidationErrors';
+import { useAuth } from '../../hooks/AuthContext';
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async (data: object) => {
-    try {
-      formRef.current?.setErrors({});
+  const { signIn } = useAuth();
 
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .required('E-mail obrigatório')
-          .email('Digite um e-mail válido'),
-        password: Yup.string().min(6, 'Senha obrigatória'),
-      });
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
 
-      await schema.validate(data, {
-        abortEarly: false,
-      });
-    } catch (error) {
-      const errors = getValidationErrors(error);
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().min(6, 'Senha obrigatória'),
+        });
 
-      formRef.current?.setErrors(errors);
-    }
-  }, []);
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        signIn({
+          email: data.email,
+          password: data.password,
+        });
+      } catch (error) {
+        const errors = getValidationErrors(error);
+
+        formRef.current?.setErrors(errors);
+      }
+    },
+    [signIn],
+  );
 
   return (
     <>
