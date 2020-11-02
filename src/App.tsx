f781e0fc4f0cf38321b1d7118c53a305c15b5a47
Kelson Palharini <kelsonpalharini@bsd.com.br>
2020-11-02 19:43:03 -0300

    Created ToastContainer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,16 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './hooks/AuthContext';
+import AppProvider from './hooks';
 
 import SignIn from './pages/SignIn';
-import ToastContainer from './components/ToastContainer';
 
 const App: React.FC = () => {
   return (
     <>
-      <AuthProvider>
+      <AppProvider>
         <SignIn />
-      </AuthProvider>
-
-      <ToastContainer />
+      </AppProvider>
 
       <GlobalStyle />
     </>
